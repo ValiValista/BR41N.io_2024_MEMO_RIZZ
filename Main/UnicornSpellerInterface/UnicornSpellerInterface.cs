71a3c7d54990c11f75b5ff016a81b8d26c197c73
ValiValista <a01647566@tec.mx>
2024-10-06 15:52:32 -0600

    HOAUSKFHJKL

---
+++ Main/UnicornSpellerInterface/UnicornSpellerInterface.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using UnityEngine;
+using System.Net;
+using Unity.ItemRecever;
+
+public class UnicornSpellerInterface : MonoBehaviour
+{
+    public string SpellerData { get; private set; }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        try
+        {
+            //IP settings
+            string ips = "127.0.0.1";
+            int port = 1000;
+            IPAddress ip = IPAddress.Parse(ips);
+
+            //Start listening for Unicorn Speller network messages
+            SpellerReceiver r = new SpellerReceiver(ip, port);
+
+            //attach items received event
+            r.OnItemReceived += OnItemReceived;
+
+            Debug.Log(String.Format("Listening to {0} on port {1}.", ip, port));
+        }
+        catch (Exception ex)
+        {
+            Debug.Log(ex.Message);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        string ips = "127.0.0.1";
+        int port = 1000;
+        IPAddress ip = IPAddress.Parse(ips);
+
+        SpellerReceiver r = new SpellerReceiver(ip, port);
+        r.OnItemReceived += OnItemReceived;
+        //Do something...
+    }
+
+    // OnItemReceived is called if a classified item is received from Unicorn Speller via udp.
+    private void OnItemReceived(object sender, EventArgs args)
+    {
+        ItemReceivedEventArgs eventArgs = (ItemReceivedEventArgs)args;
+        Debug.Log(String.Format("Received BoardItem:\tName: {0}\tOutput Text: {1}", eventArgs.BoardItem.Name, eventArgs.BoardItem.OutputText));
+
+        SpellerData = eventArgs.BoardItem.Name;
+        //Do something...
+    }
+}
